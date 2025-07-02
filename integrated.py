26c4a1d1aeab536ffd1073a6020ee7b83b0b48ca
PARTH PATHAK <71459513+parth1234560@users.noreply.github.com>
2025-07-02 21:44:22 +0530

    Update integrated.py

--- integrated.py
+++ integrated.py
@@ -182,7 +182,7 @@ with st.container():
         try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(hostname=host, username=username, password=password, timeout=5)
+            ssh.connect(hostname=host, username=username, password=password, timeout=15)
             stdin, stdout, stderr = ssh.exec_command(cmd)
             out = stdout.read().decode()
             err = stderr.read().decode()
